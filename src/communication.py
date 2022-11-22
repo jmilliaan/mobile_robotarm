88c4f84aa2a35fbf03c6978accc40b5b261d6d30
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2022-11-23 00:46:06 +0700

    add search port function
    to be tested

--- src/communication.py
+++ src/communication.py
@@ -2,13 +2,33 @@ import serial
 import time
 
 class Communication:
-    def __init__(self, port, baud_rate=115200, timeout=1):
+    def __init__(self, port=None, baud_rate=115200, timeout=1):
         self.port = port
         self.baud_rate = baud_rate
         self.timeout = timeout
-        self.ser = serial.Serial(self.port, self.baud_rate, self.timeout)
+        if port is not None:
+            self.ser = serial.Serial(self.port, self.baud_rate, self.timeout)
+        else:
+            self.search_port()
         self.ser.reset_input_buffer()
+    
+    def search_port(self):
+        n = 4
+        names = ["ACM", "USB"]
+        base = "/dev/tty"
+        for name in names:
+            for i in range(n + 1):
+                try_port = f"{base}{name}{i}"
+                print(try_port)
+                try:
+                    self.ser = serial.Serial(try_port, self.baud_rate, self.timeout)
+                    self.port = try_port
+                    print(f"Serial port found at {try_port}")
+                except serial.SerialException:
+                    continue
 
+
+        
     def write(self, message):
         self.reset()
         self.ser.write(bytes(message))
