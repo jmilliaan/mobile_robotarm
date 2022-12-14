1d65e568491286d2f0cd02f449b6cce076330541
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2022-12-14 18:10:35 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from communication import Communication
-from vision import ComputerVision
-from tools import statistics, calculate # random functions
+# from vision import ComputerVision
+# from tools import statistics, calculate # random functions
 from DataCollection.JoystickModule import getJS
 
 if __name__ == "__main__":
