1df3bd2b8a7bb0f7ae8b0f705d4ddbc815ace9ee
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2022-12-14 19:05:00 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
 # from communication import Communication
 # from vision import ComputerVision
 # from tools import statistics, calculate # random functions
-# from DataCollection.JoystickModule import getJS
+from DataCollection.JoystickModule import getJS
 from smbus import SMBus
 import serial
 from time import sleep
 addr = 0x08
 bus = SMBus(1)
 
+def labelencode_joystick(axis_data):
+    pass
+
+
 if __name__ == "__main__":
+    data = getJS('')
+    axis = [data["axis1"], data["axis2"], data["axis3"], data["axis4"]]
     while True:
+        print(axis)
         bus.write_byte(addr, 0x1)
-        sleep(1)
+        sleep(0.5)
