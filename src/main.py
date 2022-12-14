66b575991034e06778a742e2138d846052844acc
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2022-12-14 18:13:40 +0700

    update main

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from DataCollection.JoystickModule import getJS
 if __name__ == "__main__":
     for i in range(3):
         data = getJS('')
+        axis = [data["axis1"], data["axis2"], data["axis3"], data["axis4"]]
         print(data)
     # com = Communication(port="/dev/ttyACM0")
     # com.write()
