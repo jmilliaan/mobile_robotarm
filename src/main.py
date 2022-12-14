daf3bd0de5bc84269636300a5645450c6430afdc
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2022-12-14 18:31:21 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-from communication import Communication
+# from communication import Communication
 # from vision import ComputerVision
 # from tools import statistics, calculate # random functions
 from DataCollection.JoystickModule import getJS
+import serial
 from time import sleep
 if __name__ == "__main__":
-    com = Communication(port="/dev/ttyACM0", baud_rate=115200, timeout=1)
+    # com = Communication(port="/dev/ttyACM0", baud_rate=115200, timeout=1)
+    # print(com.ser.name)
+    ser = serial.Serial('/dev/ttyACM0', serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
+    print(ser.name)
     for i in range(100):
         data = getJS('')
         axis = [data["axis1"], data["axis2"], data["axis3"], data["axis4"]]
