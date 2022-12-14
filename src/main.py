ee947ce097696a94c04db9b800c32d4c293a2e6d
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2022-12-14 19:11:19 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def labelencode_joystick(axis_data):
 
 
 if __name__ == "__main__":
-    data = JoystickModule.getJS('')
+    data = getJS('')
     axis = [data["axis1"], data["axis2"], data["axis3"], data["axis4"]]
     while True:
         print(axis)
