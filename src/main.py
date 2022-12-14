de203e08c49ad33a894b14de9f5eb5e3351887b8
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2022-12-14 18:15:19 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,11 +2,12 @@ from communication import Communication
 # from vision import ComputerVision
 # from tools import statistics, calculate # random functions
 from DataCollection.JoystickModule import getJS
-
+from time import sleep
 if __name__ == "__main__":
     for i in range(3):
         data = getJS('')
         axis = [data["axis1"], data["axis2"], data["axis3"], data["axis4"]]
         print(axis)
+        sleep(0.5)
     # com = Communication(port="/dev/ttyACM0")
     # com.write()
