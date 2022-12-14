b24bd3dbd00005fa6dd12dcb7706d169c8526a52
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2022-12-14 18:02:59 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 from communication import Communication
 from vision import ComputerVision
 from tools import statistics, calculate # random functions
+from DataCollection.JoystickModule import getJS
 
+if __name__ == "__main__":
+    data = getJS('')
+    # com = Communication(port="/dev/ttyACM0")
+    # com.write()
