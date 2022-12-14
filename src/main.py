e0bb5772a0fe9fbc1eefb748e99c1855f2806a92
Joy Milliaan <jmilliaan03@students.calvin.ac.id>
2022-12-14 18:25:37 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from communication import Communication
 from DataCollection.JoystickModule import getJS
 from time import sleep
 if __name__ == "__main__":
-    com = Communication(port="/dev/ttyACM0")
+    com = Communication(port="/dev/ttyACM0", baud_rate=115200, timeout=1)
     for i in range(100):
         data = getJS('')
         axis = [data["axis1"], data["axis2"], data["axis3"], data["axis4"]]
