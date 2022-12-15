1b8232a9b3159b6136e6d057557825ed5ff2a253
hizttl <83649791+hizttl@users.noreply.github.com>
2022-12-15 17:10:08 +0700

    Update basicdrive_accel.ino

--- arduino_src/drive/basicdrive_accel/basicdrive_accel.ino
+++ arduino_src/drive/basicdrive_accel/basicdrive_accel.ino
@@ -6,17 +6,24 @@ const int dir_2 = 6;
 const int step_2 = 7;
 const int dir_3 = 8;
 const int step_3 = 9;
-const int dir_4 = 10;
-const int step_4 = 11;
+const int dir_4 = 22;
+const int step_4 = 24;
 const int motor_interface_type = 1;
 
 int motion_increment = 200;
 const int n_motors = 4;
 int pos_array[n_motors] = {
   200, 
-  -200, 
-  -200, 
+  200, 
+  200, 
   200
+};
+
+int spd_array[n_motors] = {
+  200,
+  100,
+  200,
+  100
 };
 
 AccelStepper motor_1(motor_interface_type, step_1, dir_1);
@@ -32,9 +39,11 @@ AccelStepper* steppers[n_motors] = {
 };
 void setup() {
   Serial.begin(115200);
+  motor_2.setPinsInverted(true);
+  motor_4.setPinsInverted(true);
   for(int stepper_n=0; stepper_n < n_motors; stepper_n++){
-    steppers[stepper_n]->setMaxSpeed(2000);
-    steppers[stepper_n]->setAcceleration(400);
+    steppers[stepper_n]->setMaxSpeed(1000);
+    steppers[stepper_n]->setAcceleration(200);
     }
 }
 
@@ -45,11 +54,15 @@ void loop() {
   int current_pos4 = motor_4.currentPosition();
   
   for(int stepper_n=0; stepper_n <  n_motors; stepper_n++){
+    steppers[stepper_n]->setMaxSpeed(spd_array[stepper_n]);
+    steppers[stepper_n]->setSpeed(spd_array[stepper_n]);
+  }
+  for(int stepper_n=0; stepper_n <  n_motors; stepper_n++){
     steppers[stepper_n]->move(pos_array[stepper_n]);
-    }
+  }
   for(int stepper_n=0; stepper_n <  n_motors; stepper_n++){
     steppers[stepper_n]->run();
-    }
+  }
   Serial.print(current_pos1); Serial.print(" ");
   Serial.print(current_pos2); Serial.print(" ");
   Serial.print(current_pos3); Serial.print(" ");
